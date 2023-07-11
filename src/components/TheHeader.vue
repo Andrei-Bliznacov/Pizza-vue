5c702dd6a6a084da25530bb6585b0b37c41317bf
Andrei-Bliznacov <andreii_93_93@inbox.ru>
2023-07-11 21:39:40 +0300

    pizza-vue

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+		<header class="header">
+			<a href="index.html" class="logo">
+				<img src="img/icon/logo.svg" alt="Logo" />
+			</a>
+			<label class="address">
+				<input type="text" class="input input-address" placeholder="Адрес доставки" />
+			</label>
+			<div class="buttons">
+				<span class="user-name"></span>
+				<button class="button button-primary button-auth">
+					<span class="button-auth-svg"></span>
+					<span class="button-text">Войти</span>
+				</button>
+				<button class="button button-cart" id="cart-button">
+					<span class="button-cart-svg"></span>
+					<span class="button-text">Корзина</span>
+				</button>
+				<button class="button button-primary button-out">
+					<span class="button-text">Выйти</span>
+					<span class="button-out-svg"></span>
+				</button>
+			</div>
+		</header>
+	</div>
+</template>
